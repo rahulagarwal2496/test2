1ff33d641b5464e8817e90413b0d9f00ff7ce5e3
rahulagarwal2496 <162341104+rahulagarwal2496@users.noreply.github.com>
2025-07-29 18:33:00 +0530

    Update TestApplication.java

--- src/main/java/in/psa/TestApplication.java
+++ src/main/java/in/psa/TestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		int x=10;
+		int y=20;
 		SpringApplication.run(TestApplication.class, args);
 	}
 
